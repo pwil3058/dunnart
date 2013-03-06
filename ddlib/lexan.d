78585d3cc57417fa5466a49f79e63163eb13c390
Peter Williams <pwil3058@bigpond.net.au>
2013-03-06 15:29:03 +1000

    LexicalAnalyser: first pass at a string based lexical analyser. This will not work with streams.
    Signed-off-by: Peter Williams <pwil3058@gmail.com>

---
+++ ddlib/lexan.d
@@ -0,0 +1,331 @@
+module ddlib.lexan;
+
+import std.regex;
+import std.ascii;
+// TODO: make sure LexicalAnalyser works with UTF-8 Unicode
+
+enum MatchType {bracketed, literal, regularExpression};
+
+class TokenSpec {
+    immutable string name;
+    immutable MatchType matchType;
+    union {
+        immutable string pattern;
+        immutable string[2] bracket;
+        Regex!char re;
+    }
+
+    this(string nm, string[] defs...)
+    in {
+        assert(defs.length == 1 || defs.length == 2);
+    }
+    body {
+        name = nm;
+        if (defs.length > 1) {
+            matchType = MatchType.bracketed;
+            bracket[0] = defs[0];
+            bracket[1] = defs[1];
+        } else if (defs[0][0] == '"' && defs[0][$ - 1] == '"') {
+            matchType = MatchType.literal;
+            pattern = defs[0][1 .. $ - 1];
+        } else {
+            matchType = MatchType.regularExpression;
+            re =  regex("^" ~ defs[0]);
+        }
+    }
+}
+
+unittest {
+    auto ts = new TokenSpec("TEST", "\"test\"");
+    assert(ts.name == "TEST");
+    assert(ts.matchType == MatchType.literal);
+    assert(ts.pattern == "test");
+    ts = new TokenSpec("TESTRE", "[a-zA-Z]+");
+    assert(ts.name == "TESTRE");
+    assert(ts.matchType == MatchType.regularExpression);
+    assert(!ts.re.empty);
+    ts = new TokenSpec("BRACKET", "%{", "%}");
+    assert(ts.name == "BRACKET");
+    assert(ts.matchType == MatchType.bracketed);
+    assert(ts.bracket[0] == "%{" && ts.bracket[1] == "%}");
+}
+
+class LiteralMatchNode {
+    bool validMatch;
+    LiteralMatchNode[char] tails;
+
+    this(string str) {
+        validMatch = str.length == 0;
+        if (!validMatch) {
+            tails[str[0]] = new LiteralMatchNode(str[1 .. $]);
+        }
+    }
+
+    void
+    add_tail(string new_tail) {
+        if (new_tail.length == 0) {
+            validMatch = true;
+        } else if (new_tail[0] in tails) {
+            tails[new_tail[0]].add_tail(new_tail[1 .. $]);
+        } else {
+            tails[new_tail[0]] = new LiteralMatchNode(new_tail[1 .. $]);
+        }
+    }
+}
+
+class LiteralMatcher {
+private:
+    LiteralMatchNode[char] literals;
+
+public:
+    void
+    add_literal(string literal)
+    {
+        if (literal[0] in literals) {
+            literals[literal[0]].add_tail(literal[1 .. $]);
+        } else {
+            literals[literal[0]] = new LiteralMatchNode(literal[1 .. $]);
+        }
+    }
+
+    string
+    get_longest_match(string target, size_t offset=0)
+    {
+        auto lvm = offset;
+        auto lits = literals;
+        for (auto index = offset; index < target.length && target[index] in lits; index++) {
+            if (lits[target[index]].validMatch)
+                lvm = index + 1;
+            lits = lits[target[index]].tails;
+        }
+        return target[offset .. lvm];
+    }
+}
+
+unittest {
+    auto lm = new LiteralMatcher;
+    auto test_strings = ["alpha", "beta", "gamma", "delta", "test", "tes", "tenth"];
+    auto rubbish = "rubbish";
+    foreach(test_string; test_strings) {
+        assert(lm.get_longest_match(rubbish ~ test_string) == "");
+        assert(lm.get_longest_match(rubbish ~ test_string, rubbish.length) == "");
+        assert(lm.get_longest_match(rubbish ~ test_string ~ rubbish, rubbish.length) == "");
+        assert(lm.get_longest_match(test_string ~ rubbish) == "");
+    }
+    foreach(test_string; test_strings) {
+        lm.add_literal(test_string);
+    }
+    foreach(test_string; test_strings) {
+        assert(lm.get_longest_match(test_string) == test_string);
+    }
+    foreach(test_string; test_strings ~ []) {
+        assert(lm.get_longest_match(rubbish ~ test_string) == "");
+        assert(lm.get_longest_match(rubbish ~ test_string, rubbish.length) == test_string);
+        assert(lm.get_longest_match(rubbish ~ test_string ~ rubbish, rubbish.length) == test_string);
+        assert(lm.get_longest_match(test_string ~ rubbish) == test_string);
+    }
+}
+
+struct CharLocation {
+    // Line numbers and offsets both start at 1 (i.e. human friendly)
+    // as these are used for error messages.
+    size_t lineNumber;
+    size_t offset;
+}
+
+class CharLocationData {
+    private size_t[] lineStart;
+
+    this(string text)
+    {
+        lineStart = [0];
+        if (newline.length == 1) {
+            for (size_t i; i < text.length; i++)
+                if (newline[0] == text[i])
+                    lineStart ~= (i + 1);
+        } else {
+            for (size_t i; i < text.length - newline.length + 1; i++)
+                if (newline == text[i .. i + newline.length])
+                    lineStart ~= (i + newline.length);
+        }
+    }
+
+    CharLocation
+    get_char_location(size_t index)
+    {
+        size_t imin = 0;
+        size_t imax = lineStart.length - 1;
+
+        while (imax > imin) {
+            size_t imid = (imin + imax) / 2;
+            if (lineStart[imid] < index) {
+                imin = imid + 1;
+            } else {
+                imax = imid;
+            }
+        }
+        // lineStart.length > 0 so this should hold?
+        assert(imin == imax);
+
+        // index will be on line at lineStart[imin] or the one before
+        auto ln = (index < lineStart[imin]) ? imin : imin + 1;
+        auto offset = index - lineStart[ln - 1] + 1;
+
+        return CharLocation(ln, offset);
+    }
+}
+
+class MatchResult {
+    TokenSpec tokenSpec;
+    string matchedText;
+    CharLocation location;
+
+    this(TokenSpec spec, string text, CharLocation locn)
+    {
+        tokenSpec = spec;
+        matchedText = text;
+        location = locn;
+    }
+
+    this(string text, CharLocation locn)
+    {
+        tokenSpec = null;
+        matchedText = text;
+        location = locn;
+    }
+
+    @property bool
+    is_valid_token() {
+        return tokenSpec !is null;
+    }
+}
+
+class LexicalAnalyser {
+    private LiteralMatcher literalMatcher;
+    private TokenSpec[] bracketedTokenSpecs;
+    private TokenSpec[string] literalTokenSpecs;
+    private TokenSpec[] regexTokenSpecs;
+    private string inputText;
+    private size_t index;
+    private CharLocationData charLocationData;
+
+    this(TokenSpec[] tokenSpecs) {
+        literalMatcher = new LiteralMatcher;
+        auto bcnt = 0;
+        auto lcnt = 0;
+        auto recnt = 0;
+        foreach (ts; tokenSpecs) {
+            if (ts.matchType == MatchType.bracketed) {
+                bracketedTokenSpecs ~= ts;
+                bcnt++;
+            } else if (ts.matchType == MatchType.literal) {
+                literalTokenSpecs[ts.pattern] = ts;
+                literalMatcher.add_literal(ts.pattern);
+                lcnt++;
+            } else if (ts.matchType == MatchType.regularExpression) {
+                regexTokenSpecs ~= ts;
+                recnt++;
+            }
+        }
+        assert(bcnt == bracketedTokenSpecs.length);
+        assert(lcnt == literalTokenSpecs.length);
+        assert(recnt == regexTokenSpecs.length);
+    }
+
+    void
+    set_input_text(string text) {
+        inputText = text;
+        index = 0;
+        charLocationData = new CharLocationData(text);
+    }
+
+    MatchResult advance() {
+        while (index < inputText.length) {
+            // The reported location is for the first character of the match
+            auto location = charLocationData.get_char_location(index);
+
+            // bracketed text takes precedence
+            foreach (ts; bracketedTokenSpecs) {
+                if (inputText[index .. index + ts.bracket[0].length] == ts.bracket[0]) {
+                    for (auto j = index + ts.bracket[0].length; j < inputText.length - ts.bracket[1].length + 1; j++) {
+                        if (inputText[j .. j + ts.bracket[1].length] == ts.bracket[1]) {
+                            auto mt = inputText[index .. j + ts.bracket[1].length];
+                            index += mt.length;
+                            return new MatchResult(ts, mt, location);
+                        }
+                    }
+                }
+            }
+
+            // Find longest match found by literal match or regex
+            auto llm = literalMatcher.get_longest_match(inputText, index);
+
+            auto lrem = "";
+            TokenSpec lremts;
+            foreach (tspec; regexTokenSpecs) {
+                auto m = match(inputText[index .. $], tspec.re);
+                if (m && m.hit.length > lrem.length) {
+                    lrem = m.hit;
+                    lremts = tspec;
+                }
+            }
+
+            if (llm.length && llm.length >= lrem.length) {
+                // if the matches are of equal length literal wins
+                index += llm.length;
+                return new MatchResult(literalTokenSpecs[llm], llm, location);
+            } else if (lrem.length) {
+                index += lrem.length;
+                return new MatchResult(lremts, lrem, location);
+            } else if (newline == inputText[index .. index + newline.length]) {
+                // We do white space and new line skipping last so that
+                // rules can catch them if required
+                index += newline.length;
+                continue;
+            } else if (isWhite(inputText[index])) {
+                index += 1;
+                continue;
+            } else {
+                index += 1;
+                return new MatchResult(inputText[index - 1 .. index], location);
+            }
+        }
+
+        return null;
+    }
+}
+
+unittest {
+    TokenSpec[] tslist = [
+        new TokenSpec("IF", "\"if\""),
+        new TokenSpec("IDENT", "[a-zA-Z]+[\\w_]*"),
+        new TokenSpec("LITERAL", "(\"[a-zA-Z]+[\\w_]*\")"),
+        new TokenSpec("BTEXTL", "&{", "&}"),
+        new TokenSpec("PRED", "?{", "?}"),
+    ];
+    auto la = new LexicalAnalyser(tslist);
+    la.set_input_text("if iffy\n \"quoted\" \"if\" \n9 $ \tname &{ one \n two &} and so ?{on?}");
+    MatchResult m = la.advance();
+    assert(m.tokenSpec.name == "IF" && m.matchedText == "if" && m.location.lineNumber == 1);
+    m = la.advance();
+    assert(m.tokenSpec.name == "IDENT" && m.matchedText == "iffy" && m.location.lineNumber == 1);
+    m = la.advance();
+    assert(m.tokenSpec.name == "LITERAL" && m.matchedText == "\"quoted\"" && m.location.lineNumber == 2);
+    m = la.advance();
+    assert(m.tokenSpec.name == "LITERAL" && m.matchedText == "\"if\"" && m.location.lineNumber == 2);
+    m = la.advance();
+    assert(m.tokenSpec is null && m.matchedText == "9" && m.location.lineNumber == 3);
+    m = la.advance();
+    assert(m.tokenSpec is null && m.matchedText == "$" && m.location.lineNumber == 3);
+    m = la.advance();
+    assert(m.tokenSpec.name == "IDENT" && m.matchedText == "name" && m.location.lineNumber == 3);
+    m = la.advance();
+    assert(m.tokenSpec.name == "BTEXTL" && m.matchedText == "&{ one \n two &}" && m.location.lineNumber == 3);
+    m = la.advance();
+    assert(m.tokenSpec.name == "IDENT" && m.matchedText == "and" && m.location.lineNumber == 4);
+    m = la.advance();
+    assert(m.tokenSpec.name == "IDENT" && m.matchedText == "so" && m.location.lineNumber == 4);
+    m = la.advance();
+    assert(m.tokenSpec.name == "PRED" && m.matchedText == "?{on?}" && m.location.lineNumber == 4);
+    assert(la.advance() is null);
+}
