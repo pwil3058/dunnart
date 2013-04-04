bf9054eb72c58aa64e3883f52235db6b83e9a42c
Peter Williams <pwil3058@gmail.com>
2013-04-04 16:56:26 +1000

    ddpg_bespoke: put the extra output behind a --verbose flag
    Signed-off-by: Peter Williams <pwil3058@gmail.com>

--- ddpg_bespoke.d
+++ ddpg_bespoke.d
@@ -7,9 +7,11 @@ import std.utf;
 import generated;
 import grammar;
 
+bool verbose;
+
 int main(string[] args)
 {
-    getopt(args);
+    getopt(args, "v|verbose", &verbose);
     if (args.length != 2) {
         print_usage(args[0]);
         return -1;
@@ -31,37 +33,41 @@ int main(string[] args)
     if (!parser.parse_text(inputText)) {
         return 3;
     }
-    writeln("Tokens:");
-    foreach (token; symbolTable.get_tokens_ordered()) {
-        with (token) {
-            writefln("\t%s: %s: %s: %s: %s: %s: %s", id, name, type, pattern, fieldName, associativity, precedence);
-            writefln("\t\tDefined At: %s", definedAt);
-            writefln("\t\tUsed At: %s", usedAt);
+    if (verbose) {
+        writeln("Tokens:");
+        foreach (token; symbolTable.get_tokens_ordered()) {
+            with (token) {
+                writefln("\t%s: %s: %s: %s: %s: %s: %s", id, name, type, pattern, fieldName, associativity, precedence);
+                writefln("\t\tDefined At: %s", definedAt);
+                writefln("\t\tUsed At: %s", usedAt);
+            }
         }
-    }
-    writeln("Not Terminals:");
-    foreach (token; symbolTable.get_non_terminals_ordered()) {
-        with (token) {
-            writefln("\t%s: %s:", id, name);
-            writefln("\t\tDefined At: %s", definedAt);
-            writefln("\t\tUsed At: %s", usedAt);
+        writeln("Not Terminals:");
+        foreach (token; symbolTable.get_non_terminals_ordered()) {
+            with (token) {
+                writefln("\t%s: %s:", id, name);
+                writefln("\t\tDefined At: %s", definedAt);
+                writefln("\t\tUsed At: %s", usedAt);
+            }
         }
-    }
-    writeln("Productions:");
-    for (auto i = 0; i < grammarSpecification.productionList.length; i++) {
-        writefln("\t%s:\t%s", i, grammarSpecification.productionList[i]);
+        writeln("Productions:");
+        for (auto i = 0; i < grammarSpecification.productionList.length; i++) {
+            writefln("\t%s:\t%s", i, grammarSpecification.productionList[i]);
+        }
     }
     // Generate the grammar from the specification
     auto grammar = new Grammar(grammarSpecification);
     if (grammar is null || !grammar.is_valid) {
         return 4;
     }
-    writeln("\nGrammar");
-    writeln(grammar.get_parser_states_description());
+    if (verbose) {
+        writeln("\nGrammar");
+        writeln(grammar.get_parser_states_description());
+    }
     return 0;
 }
 
 void print_usage(string command)
 {
-    writefln("Usage: %s <file>", command);
+    writefln("Usage: [--verbose|-v] %s <file>", command);
 }
